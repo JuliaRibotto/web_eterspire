378f2a31a17239d7ab6db6351335bbf8b4827b5e
Ju <ribottojulia@gmail.com>
2021-12-13 14:04:37 -0300

    heroku setup

--- src/app.js
+++ src/app.js
@@ -17,6 +17,6 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 //------------------------------
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 });
